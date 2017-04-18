09d5984aa99d5f09767b2c0cec5d21d308fc39a5
Przemyslaw Lusnia <przemyslawlusnia@gmail.com>
2017-04-18 19:00:50 +1000

    simpler Realm with try-with-resource

--- app/src/main/java/com/przemyslawlusnia/vocabularycreator/wordlist/repository/WordsRealmRepository.java
+++ app/src/main/java/com/przemyslawlusnia/vocabularycreator/wordlist/repository/WordsRealmRepository.java
@@ -1,15 +1,15 @@
 package com.przemyslawlusnia.vocabularycreator.wordlist.repository;
 
 import android.util.Log;
-import com.przemyslawlusnia.vocabularycreator.data.RealmRepository;
 import com.przemyslawlusnia.vocabularycreator.wordlist.domain.WordDomainMapper;
 import com.przemyslawlusnia.vocabularycreator.wordlist.domain.WordDomainModel;
+import io.realm.Realm;
 import io.realm.RealmResults;
 import java.util.Collections;
 import java.util.List;
 import rx.Observable;
 
-public class WordsRealmRepository extends RealmRepository implements WordsRepository {
+public class WordsRealmRepository implements WordsRepository {
 
   private static final String TAG = WordsRealmRepository.class.getSimpleName();
 
@@ -21,21 +21,18 @@ public class WordsRealmRepository extends RealmRepository implements WordsReposi
 
   @Override
   public Observable<Void> delete(List<WordDomainModel> words) {
-    openRealmIfClosed();
-    try {
+    try (Realm realmInstance = Realm.getDefaultInstance()) {
       for (int i = 0; i < words.size(); i++) {
         WordDomainModel wordDomainModel = words.get(i);
-        RealmResults<WordRealm> result = realm.where(WordRealm.class)
+        RealmResults<WordRealm> result = realmInstance.where(WordRealm.class)
             .equalTo(WordRealm.KEY_WORD, wordDomainModel.getWord())
             .equalTo(WordRealm.KEY_TRANSLATION, wordDomainModel.getTranslation())
             .findAll();
         if (result.isEmpty()) {
           Log.e(TAG, "Such word do not exists in database");
         }
-        realm.executeTransaction(realm -> result.deleteAllFromRealm());
+        realmInstance.executeTransaction(realm -> result.deleteAllFromRealm());
       }
-    } finally {
-      closeRealm(); // common Realm idiom for non-UI threads usage
     }
     return Observable.empty();
   }
@@ -43,29 +40,23 @@ public class WordsRealmRepository extends RealmRepository implements WordsReposi
   @Override
   public Observable<Boolean> add(WordDomainModel wordDomainModel) {
     final WordRealm wordRealm = WordRealm.mapToWordRealm(wordDomainModel);
-    openRealmIfClosed();
-    try {
-      realm.executeTransaction(realm -> realm.copyToRealmOrUpdate(wordRealm));
+    try (Realm realmInstance = Realm.getDefaultInstance()) {
+      realmInstance.executeTransaction(realm -> realm.copyToRealmOrUpdate(wordRealm));
     } catch (Exception e) {
       return Observable.just(false);
-    } finally {
-      closeRealm();
     }
     return Observable.just(true);
   }
 
   @Override
   public Observable<List<WordDomainModel>> getAllWords() {
-    openRealmIfClosed();
     Observable<List<WordDomainModel>> observable = Observable.just(Collections.emptyList());
-    try {
-      RealmResults<WordRealm> allWords = realm.where(WordRealm.class).findAll();
+    try (Realm realmInstance = Realm.getDefaultInstance()) {
+      RealmResults<WordRealm> allWords = realmInstance.where(WordRealm.class).findAll();
       List<WordRealm> result = allWords.subList(0, allWords.size());
       observable = Observable.just(mapper.mapToWordDomainModels(result));
     } catch (Exception e) {
       Log.e(TAG, "Cannot get words from database");
-    } finally {
-      closeRealm();
     }
     return observable;
   }
